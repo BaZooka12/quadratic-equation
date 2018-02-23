eab70e13eab0d250bbcc380a781985941a491ce9
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2018-02-23 15:54:16 +0300

    Quadratic equation: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let a, b, c, D;
+
+  let aReg = /^-?[0-9]+/;
+  let result = aReg.exec(equation);
+  a = +result[0];
+
+  let bReg = /(\-\s[0-9]+|\+\s[0-9]+)/;
+  let result2 = bReg.exec(equation);
+  b = +result2[0].replace(/\s/g, '');
+
+  let cReg = /(\-\s[0-9]+$|\+\s[0-9]+$)/;
+  let result3 = cReg.exec(equation);
+  c = +result3[0].replace(/\s/g, '');
+
+  D = Math.pow(b, 2) - 4 * a * c;
+
+  let x1 = Math.round( (-b - Math.sqrt(D)) / (2 * a) );
+  let x2 = Math.round( (-b + Math.sqrt(D)) / (2 * a) );
+
+  let roots = [x1, x2].sort((a, b) => a-b);
+  return roots;
 }
